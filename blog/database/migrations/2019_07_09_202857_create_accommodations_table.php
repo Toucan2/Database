8cfc6fa9c6c27e5f88c53a9897db5412d91e3a08
Toucan2 <serbanantonesei@yahoo.com>
2019-07-10 00:27:45 +0300

    initial commit

---
+++ blog/database/migrations/2019_07_09_202857_create_accommodations_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAccommodationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('accommodations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('Name');
+            $table->text('Description');
+            $table->string('Phone Number');
+            $table->decimal('Price');
+            $table->integer('Number of Rooms');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('accommodations');
+    }
+}
